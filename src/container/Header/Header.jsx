b308c07da386c00468b4f0a4a1fce9682fe4b2ab
AG-code32 <i.gerson@hotmail.com>
2023-03-27 20:22:41 -0500

    navbar created

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import SubHeading from '../../components/SubHeading/SubHeading.jsx'
+// import { images } from '../../constants'
+import welcome from '../../assets/welcome.png';
 import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='app__header app__wrapper section__padding'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Chase the new flavour' />
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={welcome} alt="header img" />
+      </div>
+    </div>
   )
 }
 
